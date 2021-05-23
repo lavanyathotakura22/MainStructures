4fea71db3d7a1b8a7d20047a9fdd010f64328e87
lavanyathotakura22 <83801773+lavanyathotakura22@users.noreply.github.com>
2021-05-23 21:46:21 +0530

    Update Constructor.cs

--- Constructor.cs
+++ Constructor.cs
@@ -6,15 +6,34 @@ using System.Threading.Tasks;
 
 namespace MainStructures
 {
-    class Constructor
+    class Constructorsss
     {
-        public Constructor()
+        int x;
+        //int m;
+        public Constructorsss()           //parameter less constructor
         {
             Console.WriteLine("explicit constructor");
         }
-        public static void main(String[] args)
+        static Constructorsss()
         {
-            Constructor c = new Constructor();
+            Console.WriteLine("this is static method");
+        }
+        public Constructorsss(int i)
+        {
+            x = i;
+            Console.WriteLine("explicit constructor" + i);   //parameterized constructor
+        }
+        public void display()                  //mention on method for reusing purpose
+        {
+            Console.WriteLine("value of declared x value is: " + x);
+        }
+        public static void Main(String[] args)
+        {
+            Constructorsss c = new Constructorsss();
+            Constructorsss c1 = new Constructorsss(10);
+            //Constructorsss c2 = new Constructorsss();
+            c1.display();
+            //c2.display();
         }
     }
 }
